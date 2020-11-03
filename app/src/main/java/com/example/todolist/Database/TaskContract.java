ebdcd9fd356a38cc2dee29745aa8d62a68898479
Gursimir <kgursimran648@gmail.com>
2020-11-03 17:03:48 +0530

    2 Commit

---
+++ app/src/main/java/com/example/todolist/Database/TaskContract.java
@@ -0,0 +1,17 @@
+package com.example.todolist.Database;
+
+import android.provider.BaseColumns;
+
+//This class defines constants which used to access the data in the database
+//we also need a helper class called TaskDbHelper to open the database
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.todolist.Database";
+    public static final int DB_VERSION = 1;
+
+    public static class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,21 +3,46 @@ package com.example.todolist;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.todolist.Database.TaskContract;
+import com.example.todolist.Database.TaskDbHelper;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  {
 
     private static final String TAG = "MainActivity";
+    private TaskDbHelper mHelper;
+    private ListView mTaskListView;
+    //ArrayAdapter will help populate the ListView with the data.
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing mHelper variable
+        mHelper = new TaskDbHelper(this);
+
+        //initializing mTaskListView
+        mTaskListView = (ListView) findViewById(R.id.list_todo);
+
+        //To see the updated data, you need to call the updateUI() method every time the underlying data of the app changes. So, add it in two places:
+        //In the onCreate() method, that initially shows all the data
+        //After adding a new task using the AlertDialog
+        updateUI();
     }
 
     @Override
@@ -38,8 +63,20 @@ public class MainActivity extends AppCompatActivity  {
                         .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                //line 48 to 56 adapt MainActivity to store data in the database
                                 String task = String.valueOf(taskEditText.getText());
-                                Log.d(TAG, "Task to add: " + task);
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
+                                //To see the updated data, you need to call the updateUI() method every time the underlying data of the app changes. So, add it in two places:
+                                //In the onCreate() method, that initially shows all the data
+                                //After adding a new task using the AlertDialog
+                                updateUI();
                             }
                         })
                         .setNegativeButton("Cancel", null)
@@ -50,5 +87,33 @@ public class MainActivity extends AppCompatActivity  {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //fetch all the data from the database and show it in the main view.
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this,
+                    R.layout.item_todo,// what view to use for the items
+                    R.id.task_title,// where to put the String of data
+                    taskList);// where to get all the data
+            mTaskListView.setAdapter(mAdapter);// set it as the adapter of the ListView instance
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
     }
 }
